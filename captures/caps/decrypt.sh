c19a4fa28a15b6f1209d4f4fc7f0bd3c752a056c
Michael Driscoll <michael@ulfheim.net>
2022-04-17 19:39:10 -0600

    Checkpoint, first 3 dgrams done

--- captures/caps/decrypt.sh
+++ captures/caps/decrypt.sh
@@ -18,4 +18,31 @@ rm -f /tmp/working.$$
 perl -p0777 -e 's/(\0\0)*$//gs; s/.{'${adlen}'}(.*).{16}/\1/s' < ${infile} > /tmp/working.$$
 cat /tmp/working.$$ | ./aes_128_gcm_decrypt ${iv} ${recordnum} ${key} ${recdata} ${authtag} > /tmp/out.$$
 cat /tmp/out.$$ > ${outfile}
+
+filesize=$(du -k /tmp/working.$$ | cut -f1)
+if [[ $filesize -lt 7 ]]; then
+    msg=$(cat /tmp/working.$$ | xxd -p | perl -pe 's/../& /g')
+else
+    start=$(head -c 3 /tmp/working.$$ | xxd -p | perl -pe 's/../& /g')
+    end=$(tail -c 3 /tmp/working.$$ | xxd -p | perl -pe 's/../& /g')
+    msg="$start ... $end"
+fi
+
+echo --------- start copy into template -------
+echo '$ key='$key
+echo '$ iv='$iv
+echo "### from this record"
+echo '$ recdata='$recdata
+echo '$ authtag='$authtag
+echo '$ recordnum='$recordnum
+echo "### may need to add -I and -L flags for include and lib dirs"
+echo '$ cc -o aes_128_gcm_decrypt aes_128_gcm_decrypt.c -lssl -lcrypto'
+echo '$ echo '"$msg" | xxd -r -p > /tmp/msg1
+echo '$ cat /tmp/msg1 \'
+echo '  | ./aes_128_gcm_decrypt $iv $recordnum $key $recdata $authtag \'
+echo '  | hexdump -C'
+echo
+hexdump -C /tmp/out.$$ | head
+echo --------- end copy into template -------
+
 rm -f /tmp/out.$$ /tmp/working.$$
